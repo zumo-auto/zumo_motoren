71dbd27929a17998b8b73099c11cfa97fed451b5
Len2w2 <168977585+Len2w2@users.noreply.github.com>
2024-05-05 21:32:40 +0200

    Update zumo_motoren.ino

--- zumo_motoren.ino
+++ zumo_motoren.ino
@@ -12,46 +12,50 @@ void setup()
 
 void loop()
 {
-  if (buttonB.isPressed())
+  //achteruit rijden.
+  if (buttonB.isPressed()) //Als button B wordt ingedrukt.
   {
-    for (int speed = 0; speed >= -400; speed--)
+    for (int speed = 0; speed >= -400; speed--) //Door deze loop versnellen de motoren van 0 naar een speed value van -400. Waardoor de robot achteruit rijden
     {
-      motors.setLeftSpeed(speed);
-      motors.setRightSpeed(speed);
-      delay(2);
+      motors.setLeftSpeed(speed); // Hier krijgt de linker motor zijn speed value.
+      motors.setRightSpeed(speed); // Hier krijgt de rechter motor zijn speed value.
+      delay(2); //2 miliseconde delay elke keer als hij door de loop gaat, voor langzamer optrekken en remmen.
     }
-    for (int speed = -400; speed <= 0; speed++)
+    for (int speed = -400; speed <= 0; speed++) //Door deze for loop remt hij weer af.
     {
-      motors.setLeftSpeed(speed);
-      motors.setRightSpeed(speed);
-      delay(2);
-    }
-  }
-  else if (buttonA.isPressed())
-    for (int speed = 0; speed <= 400; speed++)
-    {
-      motors.setLeftSpeed(speed);
-      motors.setRightSpeed(speed);
-      delay(2);
-    }
-    for (int speed = 400; speed >= 0; speed--)
-    {
-      motors.setLeftSpeed(speed);
-      motors.setRightSpeed(speed);
-      delay(2);
+      motors.setLeftSpeed(speed); // Hier krijgt de Linker motor zijn speed value.
+      motors.setRightSpeed(speed); // Hier krijgt de rechter motor zijn speed value.
+      delay(2); //2 miliseconde delay elke keer als hij door de loop gaat, voor langzamer optrekken en remmen.
     }
   }
-  else if (buttonC.isPressed())
-  {
-  for (int speed = 0; speed >= -400; speed--)
-  {
-    motors.setLeftSpeed(speed);
-    delay(2);
+
+  //Vooruit rijden.
+  else if (buttonA.isPressed()) // Als button A word ingedrukt
+    for (int speed = 0; speed <= 400; speed++) // Door deze loop versnellen de motoren van 0 naar een speed value van 400. Waardoor de robot vooruit rijd
+    {
+      motors.setLeftSpeed(speed); // Hier krijgt de linker motor zijn speed value.
+      motors.setRightSpeed(speed); // Hier krijgt de rechter motor zijn speed value.
+      delay(2); //2 miliseconde delay elke keer als hij door de loop gaat, voor langzamer optrekken en remmen.
+    }
+    for (int speed = 400; speed >= 0; speed--) // Door deze loop remt hij weer af.
+    {
+      motors.setLeftSpeed(speed); // Hier krijgt de rechter motor zijn speed value.
+      motors.setRightSpeed(speed); // Hier krijgt de rechter motor zijn speed value.
+      delay(2); //2 miliseconde delay elke keer als hij door de loop gaat, voor langzamer optrekken en remmen.
+    }
   }
-  for (int speed = -400; speed <= 0; speed++)
+  //Korte bocht naar rechts
+  else if (buttonC.isPressed())// Als button C wordt ingedrukt.
   {
-    motors.setLeftSpeed(speed);
-    delay(2);
+  for (int speed = 0; speed >= -400; speed--) //Door deze loop versnelt de linker motor van 0 naar -400, waardoor deze motor achteruit draait.
+  {
+    motors.setLeftSpeed(speed); // Hier krijgt de linker motor zijn speed value.
+    delay(2); //2 miliseconde delay voor langzamer optrekken en remmen.
+  }
+  for (int speed = -400; speed <= 0; speed++) //Door deze loop remt hij weer af.
+  {
+    motors.setLeftSpeed(speed); // Hier krijgt de linker motor zijn speed value.
+    delay(2); //2 miliseconde delay elke keer als hij door de loop gaat, voor langzamer optrekken en remmen.
   }
   }
 }
